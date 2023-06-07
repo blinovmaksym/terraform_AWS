5bcf40e0d83f333eec860452506b6811f779765f
blinovmaxim <blinov.maxim@gmail.com>
2023-06-07 15:48:17 +0300

    test

--- terraform_aws/scripts/install-tekton.sh
+++ terraform_aws/scripts/install-tekton.sh
@@ -13,7 +13,6 @@ kubectl apply --filename https://storage.googleapis.com/tekton-releases/pipeline
 echo "<< 3 >>----- >Install Tekton Dashboard"
 kubectl apply --filename https://storage.googleapis.com/tekton-releases/dashboard/latest/release.yaml
 
-
 # Install tasks from Tekton Hub
 echo "<< 4 >> ----- >Install tasks from Tekton Hub"
 tkn hub install task git-clone && tkn hub install task buildah && tkn hub install task kubernetes-actions

cf4a8b698d0316f441da2d808805f96865f92e0f
blinovmaxim <blinov.maxim@gmail.com>
2023-06-08 19:08:37 +0300

    test

--- terraform_aws/scripts/setup-wp.sh
+++ terraform_aws/scripts/setup-wp.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-mysql -h mysql -u root -pwordsec -P 3306 -e "\
+sleep 30
+# Сreate a database
+mysql -h $SERVER_NAME -u $USER_NAME -p$PASSWORD -P 3306 -e "\
    CREATE DATABASE IF NOT EXISTS WordPress; \
     USE WordPress; \
     CREATE TABLE IF NOT EXISTS TableforWP (id INT PRIMARY KEY, name VARCHAR(50)); \
     INSERT IGNORE INTO TableforWP (id, name) VALUES (1, 'Den'), (2, 'Max'), (3, 'Len');" 
-
+sleep 10 
 if [ -f /var/www/html/wp-config.php ]; then  
     wp plugin install contact-form-7 --activate --path=/var/www/html && \
     wp theme install hestia --path=/var/www/html && \

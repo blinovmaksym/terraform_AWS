6f2277ad21f8948d86a53ab1cf88215523da1546
blinovmaxim <blinov.maxim@gmail.com>
2023-06-08 12:17:30 +0300

    test

--- terraform_aws/scripts/setup-wp.sh
+++ terraform_aws/scripts/setup-wp.sh
@@ -5,10 +5,9 @@ mysql -h mysql -u root -pwordsec -P 3306 -e "\
     USE WordPress; \
     CREATE TABLE IF NOT EXISTS TableforWP (id INT PRIMARY KEY, name VARCHAR(50)); \
     INSERT IGNORE INTO TableforWP (id, name) VALUES (1, 'Den'), (2, 'Max'), (3, 'Len');" 
-if [  -f /var/www/html/wp-config.php ]; then  
-    wp --allow-root --path=/var/www/html plugin install contact-form-7 --activate && \
-    wp --allow-root --path=/var/www/html theme install hestia    && \
-    wp --allow-root --path=/var/www/html theme activate hestia     
+
+if [ -f /var/www/html/wp-config.php ]; then  
+    wp plugin install contact-form-7 --activate && wp theme install hestia && wp theme activate hestia
 else    
     # Configure WP-CLI and install plugins and themes
     wp core download --path=/var/www/html --allow-root && \
@@ -24,8 +23,5 @@ else
       --admin_user=dmonakh \
       --admin_password=monakh3108d \
       --admin_email=den3108@mon.com && \
-    wp --allow-root --path=/var/www/html plugin install contact-form-7 --activate && \
-    wp --allow-root --path=/var/www/html theme install twentytwentytwo  && \
-    wp --allow-root --path=/var/www/html theme activate twentytwentytwo     
+    wp plugin install contact-form-7 --activate && wp theme install twentytwentytwo && wp theme activate twentytwentytwo
 fi
-

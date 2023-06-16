a1c941dd3a76df2d50391324a365b834d8c39d70
blinovmaxim <blinov.maxim@gmail.com>
2023-06-16 16:24:01 +0300

    init

--- terraform_aws/main.tf
+++ terraform_aws/main.tf
@@ -45,9 +45,9 @@ module "eks" {
   create_kms_key            = false
   cluster_encryption_config = {}
 
-  eks_managed_node_group_defaults = {
-    ami_type = "ami-09e35c383eea7b5ec"
-  }
+  # eks_managed_node_group_defaults = {
+  #   ami_type = "ami-09e35c383eea7b5ec"
+  # }
 
   eks_managed_node_groups = {
     main = {

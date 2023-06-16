f34fad49c27d7858cbd7ba31847dcab0faf0563a
blinovmaxim <blinov.maxim@gmail.com>
2023-06-16 22:30:53 +0300

    init

--- terraform_aws/main.tf
+++ terraform_aws/main.tf
@@ -18,8 +18,8 @@ module "vpc" {
   azs                  = data.aws_availability_zones.available.names
   private_subnets      = ["172.16.1.0/24", "172.16.2.0/24", "172.16.3.0/24"]
   public_subnets       = ["172.16.4.0/24", "172.16.5.0/24", "172.16.6.0/24"]
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
+  # enable_nat_gateway   = true
+  # single_nat_gateway   = true
   enable_dns_hostnames = true
 
   public_subnet_tags = {

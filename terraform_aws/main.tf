4e124c20fbd190bd3f27531f83013b1bc9713e45
blinovmaxim <blinov.maxim@gmail.com>
2023-06-16 16:18:04 +0300

    init

--- terraform_aws/main.tf
+++ terraform_aws/main.tf
@@ -47,12 +47,8 @@ module "eks" {
 
   eks_managed_node_group_defaults = {
     ami_type = "ami-09e35c383eea7b5ec"
-
   }
 
-
-
-  
   eks_managed_node_groups = {
     main = {
       min_size     = 2

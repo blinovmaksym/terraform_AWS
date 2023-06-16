449ff412efd146a4c2d2f23b91ac632f4eb7d45d
blinovmaxim <blinov.maxim@gmail.com>
2023-06-16 18:15:20 +0300

    init

--- terraform_aws/main.tf
+++ terraform_aws/main.tf
@@ -35,7 +35,7 @@ module "vpc" {
 
 module "eks" {
   source = "terraform-aws-modules/eks/aws"
-  version = "19.5.3"
+  version = "19.15.3"
 
   cluster_name    = "${local.cluster_name}"
   cluster_version = "1.24"
